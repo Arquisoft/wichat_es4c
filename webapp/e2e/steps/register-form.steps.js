572533e561f0dead8c8681802073314e7465b0a4
Adrian Mahia <adriansporusdos@gmail.com>
2025-04-01 13:47:36 +0200

    Cambios en e2e tests

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const { defineFeature, loadFeature }=require('jest-cucumber');
+const { defineFeature, loadFeature } = require('jest-cucumber');
 const setDefaultOptions = require('expect-puppeteer').setDefaultOptions
 const feature = loadFeature('./features/register-form.feature');
 
@@ -7,45 +7,45 @@ let page;
 let browser;
 
 defineFeature(feature, test => {
-  
+
   beforeAll(async () => {
     browser = process.env.GITHUB_ACTIONS
-      ? await puppeteer.launch({headless: "new", args: ['--no-sandbox', '--disable-setuid-sandbox']})
-      : await puppeteer.launch({ headless: false, slowMo: 100 });
+      ? await puppeteer.launch({ headless: "new", args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+      : await puppeteer.launch({ headless: false, slowMo: 10 });
     page = await browser.newPage();
     //Way of setting up the timeout
-    setDefaultOptions({ timeout: 100000 })
+    setDefaultOptions({ timeout: 10000 })
 
     await page
-      .goto("http://localhost:3000/", {
+      .goto("http://localhost:3000", {
         waitUntil: "networkidle0",
       })
-      .catch(() => {});
+      .catch(() => { });
   });
 
-  test('The user is not registered in the site', ({given,when,then}) => {
-    
+  test('The user is not registered in the site', ({ given, when, then }) => {
+
     let username;
     let password;
 
-    given('An unregistered user', async () => {;
-      username = "pablo";
-      password = "pabloasw";
+    given(/^A user with name "(.*)" and password "(.*)"$/, async (user, pwd) => {
+      username = user
+      password = pwd
       await page.click('[data-testid="register-button"]');
     });
 
     when('I fill the data in the form and press submit', async () => {
       await expect(page).toFill('input[id="username"]', username);
       await expect(page).toFill('input[id="password"]', password);
-      await expect(page).toClick('button', { text: 'Add User' })
+      await page.click('[data-testid="submit-button"]');
     });
 
-    then('A confirmation message should be shown in the screen', async () => {
-        await expect(page).toMatchElement("div", { text: "User added successfully" });
+    then(/^The confirmation message "(.*)" should be shown in the screen$/, async (msg) => {
+      await expect(page).toMatchElement("div", { text: msg });
     });
   })
 
-  afterAll(async ()=>{
+  afterAll(async () => {
     browser.close()
   })
 
