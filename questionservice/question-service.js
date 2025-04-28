ef7337b648975e11d0e2779bd493f43fa0ed9e26
BrunoPrzCu <witherbs57@gmail.com>
2025-04-28 21:44:54 +0200

    Importar Question del modelo

--- questionservice/question-service.js
+++ questionservice/question-service.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { generateQuestion} = require('./question-gen');
 const connectToDatabase = require('./connection');
+const Question = require('./question-model');
 
 const app = express();
 const PORT = 8004;
